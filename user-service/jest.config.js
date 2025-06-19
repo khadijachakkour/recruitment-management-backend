45e94c203f8b450b27a733a277d205e04266253a
khadijachakkour <chakkourk4@gmail.com>
2025-06-19 14:51:29 +0100

    fix problems pipeline main.yml

--- user-service/jest.config.js
+++ user-service/jest.config.js
@@ -6,6 +6,7 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: "node",
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   collectCoverage: true,
   coverageReporters: ["text", "lcov"],
   testMatch: [
